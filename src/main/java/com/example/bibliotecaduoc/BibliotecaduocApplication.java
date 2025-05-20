c73b67c626b67e1167c52b41eedfe6c7d87f1751
Ruben B. <you@example.com>
2025-05-20 12:00:56 -0400

    Agregar comentario descriptivo en la clase principal de la aplicación

--- src/main/java/com/example/bibliotecaduoc/BibliotecaduocApplication.java
+++ src/main/java/com/example/bibliotecaduoc/BibliotecaduocApplication.java
@@ -3,6 +3,7 @@ package com.example.bibliotecaduoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//aplicacion de ejemplo para la biblioteca duoc
 @SpringBootApplication
 public class BibliotecaduocApplication {
 
